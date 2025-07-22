b187e1cde363788b2266ce5e8fb7535d81cf6a39
Yaninaponcec94 <ponce.yanina.c@gmail.com>
2025-07-22 16:39:53 -0300

    cors para vercel

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,18 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Permitir CORS solo desde tu frontend en Vercel
+  app.enableCors({
+    origin: ['https://invitaciones-sage.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-
-  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  // Escuchar en el puerto correcto (para Render)
   await app.listen(process.env.PORT || 3000);
 }
 
